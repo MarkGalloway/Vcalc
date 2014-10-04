e7c9142e168e94436f125881d0b69e748f467465
Mark Galloway <mgallowa@ualberta.ca>
2014-10-03 20:06:01 -0600

    Clean up some errors

--- symbol/vcalc/IntType.java
+++ symbol/vcalc/IntType.java
@@ -1,4 +1,10 @@
 package symbol.vcalc;
 
 public class IntType extends VcalcValue {
+
+    public IntType(Object value) {
+        super(value);
+        // TODO Auto-generated constructor stub
+    }
+    
 }
--- symbol/vcalc/VectorType.java
+++ symbol/vcalc/VectorType.java
@@ -3,5 +3,10 @@ package symbol.vcalc;
 import java.util.ArrayList;
 
 public class VectorType extends VcalcValue {
+
+    public VectorType(Object value) {
+        super(value);
+        // TODO Auto-generated constructor stub
+    }
 	
 }
